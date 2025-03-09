53fd97a4c8f16f07f14089683f1fdbb75317417a
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-03-09 20:30:50 +0330

    filter data with SSR and context.query

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,7 +1,46 @@
-import CategoriesPage from "@/components/templates/CategoriesPage"
+import CategoriesPage from "@/components/templates/CategoriesPage";
 
-export default function Categories() {
-  return (
-    <CategoriesPage/>  
-  )
+export default function Categories({ data }) {
+  console.log(data);
+  return <CategoriesPage data={data} />;
+}
+
+// filter data based on SSR
+
+export async function getServerSideProps(context) {
+  const {
+    query: { difficulty, time },
+  } = context;
+  const res = await fetch("http://localhost:4000/data");
+  const data = await res.json();
+
+  const filteredData = data.filter((item) => {
+    const difficultyResult = item.details.filter(
+      (detail) => detail.Difficulty && detail.Difficulty === difficulty
+    );
+
+    const timeResult = item.details.filter((detail) => {
+      const cookingTime = detail["Cooking Time"] || " ";
+      const [timeDetail] = cookingTime.split(" ");
+      if (time === "less" && timeDetail && +timeDetail <= 30) {
+        return detail;
+      }
+
+      if (time === "more" && timeDetail && +timeDetail > 30) {
+        return detail;
+      }
+    });
+
+    if (time && difficulty && timeResult.length && difficultyResult.length) {
+      return item;
+    } else if (!time && difficulty && difficultyResult.length) {
+      return item;
+    } else if (time && !difficulty && timeResult.length) {
+      return item;
+    }
+  });
+
+  return {
+    props: { data: filteredData },
+  };
 }
