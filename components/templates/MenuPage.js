d237a1546bedb42cda036d5bec222c9cc5545cc5
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-03-09 15:49:18 +0330

    add Card component to show fetched data from server

--- components/templates/MenuPage.js
+++ components/templates/MenuPage.js
@@ -1,4 +1,14 @@
+import Card from "../modules/Card";
+import styles from "./MenuPage.module.css"
+
 export default function MenuPage({ data }) {
   console.log(data);
-  return <div>MenuPage</div>;
+  return <div className={styles.container}>
+    <h2>Menu</h2>
+    <div className={styles.subContainer}>
+        {data.map(food => (
+            <Card key={food.id} data={food} />
+        ))}
+    </div>
+  </div>;
 }
